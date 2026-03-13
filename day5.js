5e1395e63177c8f565d1d61d02d5340f6716d096
Sohaib Ahmad <sohaib23ahmed@gmail.com>
2026-03-13 19:45:11 +0500

    message

--- day5.js
+++ day5.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+console.log("Script loaded")
+
+number1 = 100
+number2 = "100"
+
+number2 = Number(number2)
+console.log(number2, typeof number2)
+
+number3 = number2 + 2
+
+console.log(number3)
+
